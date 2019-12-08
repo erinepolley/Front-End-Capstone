89465e74d06c0757a2bc17eb4e8ce0448ad0c063
Erin Polley <erinepolley@gmail.com>
2019-12-07 21:15:33 -0600

    successfully rendered custom mapbox map using leaflet, and made it the size I wanted

--- src/Map.js
+++ src/Map.js
@@ -13,31 +13,34 @@ export default class Map extends Component {
 
   componentDidMount() {
     // create map
-    this.map = L.map('map').setView([36, -86.5], 10);
+    this.map = L.map('map').setView([36.161716102, -86.7780876], 13);
+    // 36.161716102717804 -86.77808761596681
+//mapbox://styles/erinepolley/ck3hi2fxk02l71ct4aubu0eeb
 
-    // add basemap with this base code 
-    // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    //   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //   maxZoom: 18,
-    //   id: 'mapbox.streets',
-    //   accessToken: 'YOUR MAPBOX API KEY'
-    // }).addTo(this.map);
-    L.tileLayer('https://api.mapbox.com/styles/v1/erinepolley/ck3hi2fxk02l71ct4aubu0eeb.html?fresh=true&title=copy&access_token=pk.eyJ1IjoiZXJpbmVwb2xsZXkiLCJhIjoiY2szaGg5NXlxMDJkeDNjcG9lbWhhZ2lwcyJ9.OQHXLQXUggCqa_b4pqWyZQ#12.3/36.153496/-86.779824/0', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoiZXJpbmVwb2xsZXkiLCJhIjoiY2szaGg5NXlxMDJkeDNjcG9lbWhhZ2lwcyJ9.OQHXLQXUggCqa_b4pqWyZQ'
-      }).addTo(this.map);
+    // add basemap
+    L.tileLayer(
+			'https://api.mapbox.com/styles/v1/erinepolley/ck3ruko0s17fs1cnu0eg7hgqh/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZXJpbmVwb2xsZXkiLCJhIjoiY2szaGg5NXlxMDJkeDNjcG9lbWhhZ2lwcyJ9.OQHXLQXUggCqa_b4pqWyZQ', 
+    {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoiZXJpbmVwb2xsZXkiLCJhIjoiY2szaGg5NXlxMDJkeDNjcG9lbWhhZ2lwcyJ9.OQHXLQXUggCqa_b4pqWyZQ'
+    }).addTo(this.map);
+
+  
 
 
-    navigator.geolocation.getCurrentPosition(position => {
-      const coords = position.coords;
-      this.map.setView([coords.latitude, coords.longitude], 16);
 
-      L.marker([coords.latitude, coords.longitude])
-        .bindPopup('This is your current <strong>location</strong>')
-        .addTo(this.map);
-    });
+
+
+    // navigator.geolocation.getCurrentPosition(position => {
+    //   const coords = position.coords;
+    //   this.map.setView([coords.latitude, coords.longitude], 16);
+
+    //   L.marker([coords.latitude, coords.longitude])
+    //     .bindPopup('This is your current <strong>location</strong>')
+    //     .addTo(this.map);
+    // });
 
     // log user clicks
     this.map.on('click', event => {
